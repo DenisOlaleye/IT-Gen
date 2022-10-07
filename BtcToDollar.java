dbc7ee21a4b7be7e0e3a195e71cca3d68b6e4345
DenisOlaleye <denis.olaleye@gmail.com>
2022-10-07 08:55:56 +0300

    додав вічний цикл

--- BtcToDollar.java
+++ BtcToDollar.java
@@ -1,26 +1,37 @@
-import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class BtcToDollar 
 {
 	public static void main (String [] args)
 	{
+//		оголошуємо змінні
 		double btc2doll;
 		double dollars;
-		
+		double btc1;
 		
 		Scanner input = new Scanner(System.in);
 		
+//		запитуємо у користувача вартість біткоїна
 		System.out.print("What is bitcoin price today? ");
 		btc2doll = input.nextInt();
-		System.out.print("How much $ do you have? ");
-		dollars = input.nextInt();
 		
-		double btc1 = dollars/btc2doll;
-		
-	
-		System.out.println( "You can buy " +btc1+ " BTC");
-		
+//		запускаємо нескінченний цикл в якому:
+		while (true)
+		{
+//			питаємо у користувача скільки доларів він має
+			System.out.print("How much $ do you have? ");		
+			dollars = input.nextInt();
+			
+//			завдяки цьому блоку ми зможемо припинати виконання програми ввівши 0
+			if (dollars == 0)
+				break;
+			
+//			рахуємо кількість віткоїнів і виводимо на екран
+			btc1 = dollars/btc2doll;
+			System.out.println( "You can buy " +btc1+ " BTC");
+			
+		}	
+				
 	}
 	
 }
